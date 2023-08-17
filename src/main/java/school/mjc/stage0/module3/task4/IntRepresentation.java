7b6161b5907eb27494eb5dc8aa42c0975784e882
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-18 02:53:12 +0400

    update

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+
+        int charC = 'c';
+        int charH = 'h';
+        int charA = 'a';
+        int charR = 'r';
+
+        System.out.println((char) charC);
+        System.out.println((char) charH);
+        System.out.println((char) charA);
+        System.out.println((char) charR);
+
     }
 }
