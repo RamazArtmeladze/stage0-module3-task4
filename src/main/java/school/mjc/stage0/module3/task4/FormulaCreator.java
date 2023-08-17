3a92095622a17786f74323c943785296eafe54ba
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-18 03:10:04 +0400

    update

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -3,14 +3,12 @@ package school.mjc.stage0.module3.task4;
 public class FormulaCreator {
     public static void main(String[] args) {
 
-        double a = 0; // Assign the value you want for 'a'
-        double b = 0; // Assign the value you want for 'b'
+        double a = 10;
+        double b = 20;
 
         double result = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
 
-        boolean followsOrderOfOperations = result == (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
-
-        System.out.println("Follows order of operations: " + followsOrderOfOperations);
+        System.out.println( result);
 
     }
 }
